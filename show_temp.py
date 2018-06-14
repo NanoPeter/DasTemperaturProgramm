53a489cb061519f20c9c23adb9dfe7e212e8890b
localuser <localuser@aghuth.de>
2018-06-14 08:52:56 +0200

    now show if device is ramping

--- show_temp.py
+++ show_temp.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from scientificdevices.lakeshore.model340 import Model340
+from scientificdevices.lakeshore.model340 import Model340, RampStatus
 
 
 if __name__ == '__main__':
     temperature_device = Model340(address=12)
 
     temperature = temperature_device.get_temperature()
+    ramp_status = temperature_device.get_rampstatus()
 
-    print('Current temperature is: {:.2f} K'.format(temperature))
+    print('current temperature is: {:.2f} K ({})'.format(temperature), ramp_status.name)
 
